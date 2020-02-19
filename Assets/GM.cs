0ebe35411eb76c7df2334eb2eb33eb7a774492c5
JanakaSandaruwan <janakasandaruwan1996@gmail.com>
2020-02-19 11:18:21 +0530

    initial commit

---
+++ Assets/GM.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    public static float vertVel =0;
+    public static int zveladj=1;
+    public static float time;
+    public static int coinCount = 0;
+    public static string lvlcompstatus="";
+    public float waitload = 0;
+
+    public float zScenePos = 52; 
+    public Transform bbPit;
+    public Transform bbNoPit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate (bbNoPit,new Vector3(0,1.95f,36),bbNoPit.rotation);
+        Instantiate (bbNoPit,new Vector3(0,1.95f,40),bbNoPit.rotation);
+        Instantiate (bbPit,new Vector3(0,1.95f,44),bbNoPit.rotation);
+        Instantiate (bbPit,new Vector3(0,1.95f,48),bbNoPit.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zScenePos < 120){
+            Instantiate (bbNoPit,new Vector3(0,1.95f,zScenePos),bbNoPit.rotation);
+            zScenePos +=4;
+        }
+
+        if (lvlcompstatus == "Fail") {
+            waitload += Time.deltaTime;
+        }
+
+        if (waitload > 2) {
+            Application.LoadLevel(1);
+        }
+    }
+}
---
+++ Assets/moveorb.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class moveorb : MonoBehaviour
+{
+    public KeyCode moveL;
+    public KeyCode moveR;
+
+    public float horizVel = 0; 
+    public string controlledLock = "n";
+    public int laneNum =2 ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody> ().velocity = new Vector3(horizVel,GM.vertVel,4);
+
+        if (Input.GetKeyDown(moveL) && laneNum > 1 && controlledLock == "n") {
+            horizVel = -2;
+            StartCoroutine(stopSlide());
+            laneNum -= 1;
+            controlledLock = "y";
+        }
+
+        if (Input.GetKeyDown(moveR) && laneNum < 3 && controlledLock == "n" ) {
+            horizVel = 2;
+            StartCoroutine(stopSlide());
+            laneNum += 1;
+            controlledLock = "y";
+        }
+    }
+
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds (.5f) ;
+        horizVel = 0;
+        controlledLock = "n";
+    }
+
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.tag == "lethal"){
+            GM.zveladj=0;
+            Destroy(gameObject);
+            GM.lvlcompstatus="Fail";
+        }
+
+        if (other.gameObject.name == "Capsule"){
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.name == "rampbottomtrig"){
+            GM.vertVel =2;
+        }
+
+        if (other.gameObject.name == "ramptoptrig"){
+            GM.vertVel =0;
+        }
+
+        if (other.gameObject.name == "exit"){
+            // UnityEngine.ScenceManagement.SceneManager.LoadScene("levelcomp");
+            Application.LoadLevel(1);
+        }
+
+        if (other.gameObject.name == "coin"){
+            GM.coinCount =+1;
+            Destroy (other.gameObject);
+        }
+    }
+}
